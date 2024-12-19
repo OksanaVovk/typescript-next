decb93fcbf090489b68156f76920042bdcaf9780
Vovk Oksana <oksvovk85@gmail.com>
2024-12-19 12:47:30 +0200

    Code

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -10,12 +10,17 @@ interface IPrors{
 
 const TaskItem: React.FC<IPrors> = ({ task, onRemove, onToggle }) => {
 
-    const onClickBtn = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.target.nextSibling.style.textDecoration = event.target.checked ? "line-through" : "none";
+const onClickBtn = (event: React.MouseEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement; // HTMLInputElement для input
+    const nextSibling = target.nextSibling as HTMLElement;
+
+    if (nextSibling) {
+        nextSibling.style.textDecoration = target.checked ? "line-through" : "none";
     }
+};
     
     return (<li className="task-item">
-        <input className="task-item-checkbox" type="checkbox" name="checkbox" defaultChecked={task.completed} onChange={()=>onToggle(task.id)} onClick={(event: React.MouseEvent<HTMLButtonElement>)=>onClickBtn(event)} />
+        <input className="task-item-checkbox" type="checkbox" name="checkbox" defaultChecked={task.completed} onChange={()=>onToggle(task.id)} onClick={(event: React.MouseEvent<HTMLInputElement>)=>onClickBtn(event)} />
         <p className="task-item-text" style={{ textDecoration: task.completed? "line-through": "none"}} >{task.text}</p>
         <button className="task-item-remove-button" onClick={()=>onRemove(task.id)}>Remove</button>
         </li>
