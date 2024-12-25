696f32a2b6571faec27e17209e01471e14572364
Vovk Oksana <oksvovk85@gmail.com>
2024-12-25 14:18:52 +0200

    Changed some files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/typescript-next",
   experimental: {
     appDir: true, // Увімкнення підтримки директорії `app`
   },
